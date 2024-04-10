62c5acf61b7f47728d9ee07414b8e90f667c9a6c
Bruno Grigoletti Laitano <bruno.grigoletti.laitano@gmail.com>
2024-04-10 13:36:12 +0000

    Working with SpringBoot

---
+++ BackendSpringBoot/backendspringboot/src/test/java/br/pucrs/bruno/laitano/backendspringboot/BackendspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.bruno.laitano.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
