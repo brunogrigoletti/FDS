33dafbdba9e89efdbe2618b7a7dea8d04aa03c1b
Bruno Laitano <bruno.grigoletti.laitano@gmail.com>
2024-04-11 22:21:51 -0300

    Código refatorado

--- SpringBoot/springboot/src/main/java/br/pucrs/bruno/laitano/springboot/Livro.java
+++ SpringBoot/springboot/src/main/java/br/pucrs/bruno/laitano/springboot/Livro.java
@@ -17,41 +17,20 @@ public class Livro {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getTitulo() {
         return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public String getAutor() {
         return autor;
     }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
     public int getAno() {
         return ano;
     }
 
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
     @Override
     public String toString() {
-        return "Livro{" +
-                "id=" + id +
-                ", titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                ", ano=" + ano +
-                '}';
+        return "Livro [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", ano=" + ano + "]";
     }
 }
